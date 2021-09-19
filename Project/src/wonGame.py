b20d08713736535cc1a49ad03d1ee68a5296cba0
Marlon Valencia <marlondvalencia@gmail.com>
2021-09-18 22:27:08 -0500

    DjangoIncorporado

--- Project/src/wonGame.py
+++ Project/src/wonGame.py
@@ -7,13 +7,13 @@ class wonGame(game):
     ruta = "winners/"+jugador.userName+".txt"
     if os.path.exists(ruta):
       f = open(ruta,"a")
-      text = +str(jugador.score)+";"+jugador.userName+"\n"
+      text = str(jugador.score)+";"+jugador.userName+"\n"
       f.write(text)
       f.close()
       print (f"Felicidades {jugador.userName}, ganaste! 30000 Puntos han sido añadidos a tu registro!")
     else:
       f = open(ruta,"w")
-      text = jugador.userName+";"+str(jugador.score)+"\n"
+      text = str(jugador.score)+";"+jugador.userName+"\n"
       f.write(text)
       f.close()
       print (f"Felicidades {jugador.userName}, ganaste! has sido añadido al registro de ganadores con 30000 puntos")
